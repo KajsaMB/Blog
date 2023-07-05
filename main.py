23481043e079054c7a72ac832dc3320f90edf223
Kajsa <Kajsa08@gmail.com>
2023-07-05 15:49:56 +0100

    replace boostrap/base with base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash, abort
-from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
+from flask_bootstrap import Bootstrap5
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -14,12 +14,12 @@ import random
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
-Bootstrap(app)
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bootstrap = Bootstrap5(app)
 
 
 #Login Manager
